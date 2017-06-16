8e59f09539bcb74a1f2947e6e31d798928b797f3
grvahuja205 <grvahuja205@gmail.com>
2017-06-16 14:52:24 +0530

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -8,7 +8,7 @@ dangal = media.Movie(
     "A Story Of Women Wrestling In India",
     "https://upload.wikimedia.org/wikipedia/en/9/99/Dangal_Poster.jpg",
     "https://www.youtube.com/watch?v=x_7YlGv9u1g"
-                    )
+    )
 
 # A Movie Object.
 rustom = media.Movie(
@@ -16,7 +16,7 @@ rustom = media.Movie(
     "A story about a navy man fighting for navy's pride against seniors",
     r"https://upload.wikimedia.org/wikipedia/en/9/96/Akshay_Kumar%27s-Rustom_poster.jpg",  # NOQA
     "https://www.youtube.com/watch?v=L83qMnbJ198"
-                    )
+    )
 
 # A Movie Object.
 lunchbox = media.Movie(
@@ -24,10 +24,10 @@ lunchbox = media.Movie(
     "A story about a man and his lunchbox",
     "https://upload.wikimedia.org/wikipedia/en/8/81/The_Lunchbox_poster.jpg",
     "https://www.youtube.com/watch?v=sK3R0rvnlPs"
-                    )
+    )
 
 # List of moviess to be passed as argument for the function to dispaly movies.
 movies = [dangal, rustom, lunchbox]
 
-# Calling the function which will creat a Web page and display the movies
+# Calling the function which will create a Web page and display the movies
 fresh_tomatoes.open_movies_page(movies)
